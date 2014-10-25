51262aee3956781cd7023ec61d98b2a9addc63be
muscly <musclyhc@gmail.com>
2014-10-26 01:28:24 +0900

    Clean On Save

---
+++ Assets/AltProg/CleanEmptyDir/Editor/Core.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AltProg.CleanEmptyDir
+{
+    [InitializeOnLoad]
+    public class Core : UnityEditor.AssetModificationProcessor
+    {
+        const string CLEAN_ON_SAVE_KEY = "k1";
+        static bool cleanOnSave;
+
+        // UnityEditor.AssetModificationProcessor
+        public static string[] OnWillSaveAssets(string[] paths)
+        {
+            if ( CleanOnSave )
+            {
+                List<DirectoryInfo> emptyDirs;
+                FillEmptyDirList( out emptyDirs );
+                DeleteAllEmptyDirAndMeta( ref emptyDirs );
+                AssetDatabase.Refresh();
+            }
+
+            return paths;
+        }
+
+
+        public static bool CleanOnSave
+        {
+            get 
+            {
+                return EditorPrefs.GetBool( CLEAN_ON_SAVE_KEY, false );
+            }
+            set
+            {
+                EditorPrefs.SetBool( CLEAN_ON_SAVE_KEY, value );
+            }
+        }
+
+
+        public static void DeleteAllEmptyDirAndMeta( ref List<DirectoryInfo> emptyDirs )
+        {
+            foreach (var dirInfo in emptyDirs)
+            {
+                try
+                {
+                    dirInfo.Delete();
+                }
+                catch( Exception e )
+                {
+                    Debug.LogException ( e );
+                }
+
+                var metaFilePath = GetMetaFilePath( dirInfo.FullName );
+                try
+                {
+                    File.Delete( metaFilePath );
+                }
+                catch( Exception e )
+                {
+                    Debug.LogException ( e );
+                }
+            }
+
+            emptyDirs = null;
+        }
+
+        public static void FillEmptyDirList( out List<DirectoryInfo> emptyDirs )
+        {
+            var newEmptyDirs = new List<DirectoryInfo>();
+            emptyDirs = newEmptyDirs;
+
+            var assetDir = new DirectoryInfo(Application.dataPath);
+
+            WalkDirectoryTree(assetDir, ( dirInfo, areSubDirsEmpty ) =>
+            {
+                bool isDirEmpty = areSubDirsEmpty && DirHasNoFile (dirInfo);
+                if ( isDirEmpty )
+                    newEmptyDirs.Add(dirInfo);
+                return isDirEmpty;
+            });
+        }
+
+        // return: Is this directory empty?
+        delegate bool IsEmptyDirectory( DirectoryInfo dirInfo, bool areSubDirsEmpty );
+
+        // return: Is this directory empty?
+        static bool WalkDirectoryTree(DirectoryInfo root, IsEmptyDirectory pred)
+        {
+            DirectoryInfo[] subDirs = root.GetDirectories();
+
+            bool areSubDirsEmpty = true;
+            foreach (DirectoryInfo dirInfo in subDirs)
+            {
+                if ( false == WalkDirectoryTree(dirInfo, pred) )
+                    areSubDirsEmpty = false;
+            }
+
+            bool isRootEmpty = pred(root, areSubDirsEmpty);
+            return isRootEmpty;
+        }
+
+        static bool DirHasNoFile(DirectoryInfo dirInfo)
+        {
+            FileInfo[] files = null;
+
+            try
+            {
+                files = dirInfo.GetFiles("*.*");
+                files = files.Where ( x => ! IsMetaFile(x.Name)).ToArray ();
+            } 
+            catch (Exception)
+            {
+            } 
+
+            return files == null || files.Length == 0;
+        }
+
+        public static string GetRelativePath(string filespec, string folder)
+        {
+            Uri pathUri = new Uri(filespec);
+            // Folders must end in a slash
+            if (!folder.EndsWith(Path.DirectorySeparatorChar.ToString()))
+            {
+                folder += Path.DirectorySeparatorChar;
+            }
+            Uri folderUri = new Uri(folder);
+            return Uri.UnescapeDataString(folderUri.MakeRelativeUri(pathUri).ToString().Replace('/', Path.DirectorySeparatorChar));
+        }
+
+        static string GetMetaFilePath(string dirPath)
+        {
+            // TODO: remove ending slash
+            return dirPath + ".meta";
+        }
+
+        static bool IsMetaFile(string path)
+        {
+            return path.EndsWith(".meta");
+        }
+    }
+}
+    
+    
---
+++ Assets/AltProg/CleanEmptyDir/Editor/MainWindow.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace AltProg.CleanEmptyDir
+{
+    public class MainWindow : EditorWindow
+    {
+        List<DirectoryInfo> emptyDirs;
+        Vector2 scrollPosition;
+		bool lastCleanOnSave;
+
+        [MenuItem("Window/AltProg Clean Empty Dir")]
+        public static void ShowWindow()
+        {
+            var w = GetWindow<MainWindow>();
+            w.title = "Clean";
+        }
+
+		void OnEnable()
+		{
+            lastCleanOnSave = Core.CleanOnSave;
+		}
+
+		void OnDisable()
+		{
+            Core.CleanOnSave = lastCleanOnSave;
+		}
+
+        void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+            {
+				bool cleanOnSave = GUILayout.Toggle ( lastCleanOnSave, "Clean Empty Dirs Automatically On Save" );
+				if ( cleanOnSave != lastCleanOnSave )
+                {
+					lastCleanOnSave = cleanOnSave;
+                    Core.CleanOnSave = cleanOnSave;
+				}
+
+				EditorGUILayout.BeginHorizontal();
+				{
+	                if (GUILayout.Button("Find Empty Dirs"))
+	                {
+	                    Core.FillEmptyDirList( out emptyDirs );
+	                }
+
+	                if ( emptyDirs == null || emptyDirs.Count == 0 )
+	                {
+						GUI.enabled = false;
+					}
+
+					Color old = GUI.color;
+					GUI.color = Color.red;
+                    if (GUILayout.Button("Delete All"))
+                    {
+                        Core.DeleteAllEmptyDirAndMeta( ref emptyDirs );
+						AssetDatabase.Refresh();
+                    }
+					GUI.color = old;
+
+					GUI.enabled = true;
+				}
+				EditorGUILayout.EndHorizontal();	
+
+
+
+                if (emptyDirs != null)
+                {
+                    scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.ExpandWidth(true));
+                    {
+                        EditorGUILayout.BeginVertical();
+                        {
+
+                            foreach (var dirInfo in emptyDirs)
+                            {
+                                GUILayout.Label( Core.GetRelativePath( dirInfo.FullName, Application.dataPath ) );
+                            }
+
+                        }
+                        EditorGUILayout.EndVertical();
+
+                    }
+                    EditorGUILayout.EndScrollView();
+                }
+
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+
+
+
+
+    }
+
+}
